6992200b0adb142e7d1679a7786711bcabfd60b1
Training_C2a.05.01 <Training_C2a.05.01@accenture.com>
2018-03-29 12:47:11 +0530

    added class file

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 
-	public static void main(string[] args){
-		system.out.println("Hello KRG");
+	public static void main(String[] args){
+		System.out.println("Hello KRG");
 	}
 
 }
